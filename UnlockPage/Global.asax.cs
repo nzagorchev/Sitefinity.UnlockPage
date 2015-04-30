aced1c0c401fb7b92cc6f53f96a3189c3a186189
nzagorchev <nikola.zagorchev@gmail.com>
2015-04-30 18:43:08 +0300

    Added global application class

---
+++ UnlockPage/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SitefinityWebApp.UnlockPage;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Telerik.Sitefinity.Abstractions.Bootstrapper.Initialized += new EventHandler<Telerik.Sitefinity.Data.ExecutedEventArgs>(Bootstrapper_Initialized);
+        }
+
+        protected void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs args)
+        {           
+            if (args.CommandName == "Bootstrapped")
+            {               
+                UnlockPageHelperExtensions.Install();
+            }
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
